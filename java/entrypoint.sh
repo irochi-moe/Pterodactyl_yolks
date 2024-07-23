1d98e5c223bb621d3c4234012791bdaebbfab73d
Irochi <me@irochi.moe>
2024-07-23 19:53:28 +0900

    Cleanup output (temp)

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -33,17 +33,17 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-# Print Java version
-printf "\033[1m\033[33mcontainer@irochi-moe~ \033[0mjava -version\n"
-java -version
+# # Print Java version
+# printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+# java -version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@irochi-moe~ \033[0m%s\n" "$PARSED"
+# # Display the command we're running in the output, and then execute it with the env
+# # from the container itself.
+# printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 eval ${PARSED}
